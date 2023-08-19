ab6565015ff387591c2ae48f14c4de7d0f8e8de7
dhaniel <65146994+aritonang92@users.noreply.github.com>
2023-08-19 19:44:38 +0700

    remove pip install streamlit line

--- un_nouveau_streamlit.py
+++ un_nouveau_streamlit.py
@@ -7,7 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1dLkZawlLgOML85Y5BGa3EzpwaXMdgnOY
 """
 
-pip install streamlit
 import streamlit as st
 
 st.write('Hello world!!')
